45234632c897363c3f57891e95df8b4085237321
Soodeya <levart.klara@gmail.com>
2026-05-24 22:41:55 +0200

    Vzpoztavljena PB
    🎊

---
+++ data/www/footer.php
@@ -0,0 +1,8 @@
+<footer class="site-footer">
+  <div class="container"><div class="row g-4">
+    <div class="col-md-3"><h6>Hiša vin Mak</h6><p>Vodole 3c<br>2229 Malečnik</p></div>
+    <div class="col-md-3"><h6>Delovnik</h6><p>Petek in sobota: 10:00–22:00<br>Nedelja in prazniki: 15:00-20:00</p></div>
+    <div class="col-md-3"><h6>Prodaja vin</h6><p>Andrej Mak<br>041 423 815<br>andrej.mak@gmail.com</p></div>
+    <div class="col-md-3"><h6>Rezervacije</h6><p>Mateja Tomažič<br>031 404 110<br>matejamak@gmail.com</p></div>
+  </div><hr class="border-secondary mt-4"><p class="text-center small mb-0">© 2025 Hiša vin Mak</p></div>
+</footer>
---
+++ data/www/form.php
@@ -0,0 +1,31 @@
+<?php
+
+include 'db.php';
+
+$ime = $_POST['ime'];
+$email = $_POST['email'];
+$telefon = $_POST['telefon'];
+$sporocilo = $_POST['sporocilo'];
+
+$sql = "INSERT INTO stranka
+(ime, email, telefon)
+
+VALUES
+('$ime', '$email', '$telefon')";
+
+$conn->exec($sql);
+
+$id_stranka = $conn->lastInsertId();
+
+$sql2 = "INSERT INTO rezervacija
+(sporocilo, id_stranka)
+
+VALUES
+('$sporocilo', '$id_stranka')";
+
+$conn->exec($sql2);
+
+header("Location: kontakt.php?success=1");
+exit();
+
+?>
