8ec3e49f14a23ff5beaa17ffa01e4b8d68f637db
MajaKarcovnik <maja.karcovnik@student.um.si>
2026-05-25 12:02:47 +0200

    Popravljeno

---
+++ data/www/db.php
@@ -0,0 +1,24 @@
+<?php
+
+$servername = "podatkovna-baza";
+$username = "root";
+$password = "superVarnoGeslo";
+$dbname = "vinogradnistvo";
+
+try {
+
+    $conn = new PDO(
+        "mysql:host=$servername;dbname=$dbname",
+        $username,
+        $password
+    );
+
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch(PDOException $e) {
+
+    die("Napaka pri povezavi: " . $e->getMessage());
+
+}
+
+?>
